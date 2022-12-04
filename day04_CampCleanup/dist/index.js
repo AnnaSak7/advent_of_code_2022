40d7d7a5d7a525d3123477a32891658c84ed9206
Anna Sakurai <anna.sakurai@futurice.com>
2022-12-04 21:14:10 +0100

    Finish day4 part2

--- day04_CampCleanup/dist/index.js
+++ day04_CampCleanup/dist/index.js
@@ -25,6 +25,7 @@ function numberOfRangeFullyContained(data) {
     const pairArrays = pair2arrays(dataInArray);
     let containedCount = 0;
     for (let i = 0; i < pairArrays.length; i++) {
+        // [48-48, 47-48]
         if (+pairArrays[i][0] === +pairArrays[i][1] &&
             +pairArrays[i][2] <= +pairArrays[i][0] &&
             +pairArrays[i][0] <= +pairArrays[i][3]) {
@@ -48,3 +49,26 @@ function numberOfRangeFullyContained(data) {
     return containedCount;
 }
 numberOfRangeFullyContained(data_1.input);
+//==============part 2===================
+function overlapAtAll(data) {
+    const dataInArray = stringDataToArrayOfArrays(data);
+    const pairArrays = pair2arrays(dataInArray);
+    let containedCount = 0;
+    for (let i = 0; i < pairArrays.length; i++) {
+        let firstMin = Math.min(+pairArrays[i][0], +pairArrays[i][2]);
+        if (+pairArrays[i][0] === +pairArrays[i][2]) {
+            containedCount += 1;
+        }
+        else if (firstMin === +pairArrays[i][0] &&
+            +pairArrays[i][2] <= +pairArrays[i][1]) {
+            containedCount += 1;
+        }
+        else if (firstMin === +pairArrays[i][2] &&
+            +pairArrays[i][0] <= +pairArrays[i][3]) {
+            containedCount += 1;
+        }
+    }
+    console.log("count in overlap ", containedCount);
+    return containedCount;
+}
+overlapAtAll(data_1.input);
--- day04_CampCleanup/src/index.ts
+++ day04_CampCleanup/src/index.ts
@@ -26,6 +26,7 @@ function numberOfRangeFullyContained(data: string): number {
   const pairArrays = pair2arrays(dataInArray);
   let containedCount = 0;
   for (let i = 0; i < pairArrays.length; i++) {
+    // [48-48, 47-48]
     if (
       +pairArrays[i][0] === +pairArrays[i][1] &&
       +pairArrays[i][2] <= +pairArrays[i][0] &&
@@ -55,3 +56,30 @@ function numberOfRangeFullyContained(data: string): number {
 }
 
 numberOfRangeFullyContained(input);
+
+//==============part 2===================
+function overlapAtAll(data: string): number {
+  const dataInArray = stringDataToArrayOfArrays(data);
+  const pairArrays = pair2arrays(dataInArray);
+  let containedCount = 0;
+  for (let i = 0; i < pairArrays.length; i++) {
+    let firstMin = Math.min(+pairArrays[i][0], +pairArrays[i][2]);
+    if (+pairArrays[i][0] === +pairArrays[i][2]) {
+      containedCount += 1;
+    } else if (
+      firstMin === +pairArrays[i][0] &&
+      +pairArrays[i][2] <= +pairArrays[i][1]
+    ) {
+      containedCount += 1;
+    } else if (
+      firstMin === +pairArrays[i][2] &&
+      +pairArrays[i][0] <= +pairArrays[i][3]
+    ) {
+      containedCount += 1;
+    }
+  }
+  console.log("count in overlap ", containedCount);
+  return containedCount;
+}
+
+overlapAtAll(input);
